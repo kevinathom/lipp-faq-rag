196b5823a642c116bf582e94dd1932ea0d620592
Kevin A Thomas <44673990+kevinathom@users.noreply.github.com>
2025-05-06 14:09:09 -0400

    Update rag_system.py
    Adapted rag_system.py to load environment dependencies and call other python files in sequence. This removes the need to run them as part of retrieval_system.py and clarifies the process across multiple files.

--- code/rag_system.py
+++ code/rag_system.py
@@ -2,55 +2,22 @@
 """
 Created on Mon Apr 28 11:54:17 2025
 @author: kevinathom
-Purpose: Run the RAG system
+Purpose: Initialize each component of the RAG system
 """
 
 # Dependencies
-## Environment
-#from dotenv import load_dotenv # For standalone testing
-#from pathlib import Path # For standalone testing
+from dotenv import load_dotenv
+from pathlib import Path
 
-#dir_project = Path('./GitHub/lipp-faq-rag') # Repository directory | # For standalone testing
-#load_dotenv(dotenv_path=dir_project / '.env') # See HF_TOKEN | # For standalone testing
-
-## Model
-#from huggingface_hub import login # For standalone testing
-from huggingface_hub import InferenceClient
-
-# Query (either text examples or from user_interface.py)
-#query = 'Where can I find market research reports?'
-#query = 'Get market size on medical implants for diabetes'
-#query = """I’m a graduate student at Upenn, and I’m currently researching Orolay (the apparel company known for its “Amazon coat”). I’m trying to find the following information, ideally from 2012 to 2025 if available: Annual revenue (sales) of Orolay; Best-selling product(s); Information on Orolay’s main customer"""
-#query = "can't access Pitchbook"
-#query = 'microsoft'
-#query = 'Am I an orange?'
-query = input_text # from user_interface.py
-print('query complete') # For UI crash testing
-
-# Vector query response
-response = query_engine.query(query)
-print('response complete') # For UI crash testing
+dir_project = Path('./GitHub/lipp-faq-rag') # Repository directory
+load_dotenv(dotenv_path=dir_project / '.env') # See HF_TOKEN
 
 
-# RAG context for LLM prompt
-context = 'Context:\n'
-for k in range(top_k):
-  context = context + response.source_nodes[k].text + '\n\n'
-print('context complete') # For standalone testing
+# Generate retrieval system
+exec(open(dir_project / 'code' / 'llm_func.py').read())
 
+# Create functions for prompt generation and LLM call
+exec(open(dir_project / 'code' / 'llm_func.py').read())
 
-# LLM prompt
-ragless_prompt = f'[INST] As a virtual business research librarian, communicate in clear, concise, accessible language. Include a link from the provided context for each suggested resource. If the provided context is irrelevant, suggest sending business research questions to a Lippincott Business Librarian instead of suggesting resources.\n\nPlease respond to this request: {query}[/INST]'
-print('ragless_prompt complete') # For UI crash testing
-ragful_prompt = ragless_prompt + context
-print('ragful_prompt complete') # For UI crash testing
-
-
-# Prompt the LLM
-client = InferenceClient(model='meta-llama/Llama-3.1-405B-Instruct', provider='nebius') # Llama 3.1-405B-Instruct
-print('specified client') # For UI crash testing
-
-completion = client.chat.completions.create(messages=[{'role': 'user', 'content': ragful_prompt}],)
-print('completion complete') # For UI crash testing
-completion_markdown = completion.choices[0].message.content
-#print(completion_markdown) # For standalone testing
+# Run user interface
+exec(open(dir_project / 'code' / 'user_interface.py').read())
