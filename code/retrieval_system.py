082440ef2a40df7c34bd2ea60b64ce91b0e839bb
Kevin A Thomas <44673990+kevinathom@users.noreply.github.com>
2025-05-06 12:22:57 -0400

    Update retrieval_system.py
    Removed irrelevant dependencies (had been commented out)

--- code/retrieval_system.py
+++ code/retrieval_system.py
@@ -16,15 +16,12 @@ load_dotenv(dotenv_path=dir_project / '.env') # See HF_TOKEN
 
 ## Retrieval
 from huggingface_hub import login
-#from huggingface_hub import InferenceClient
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
 #from llama_index.core.node_parser import HTMLNodeParser # For HTML parsing
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.postprocessor import SimilarityPostprocessor
-#from transformers import pipeline
-#import urllib.request
 
 
 # Load documents
